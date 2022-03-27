512e739c7cc530aa693e3e0ffa68c3adb2171303
manapixels <chrenelias@outlook.com>
2022-03-27 17:21:39 +0800

    Fix link styling

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -8,17 +8,17 @@ const SideNav = () => {
          <Box className="nav-section">
             <Box className="header">FIND</Box>
             <NavLink to="/guides">Local tour guides</NavLink>
-            <NavLink to="/guides">Local tours</NavLink>
-            <NavLink to="/guides">Accommodations</NavLink>
-            <NavLink to="/guides">Travel buddies</NavLink>
-            <NavLink to="/guides">Drivers</NavLink>
+            <NavLink to="/">Local tours</NavLink>
+            <NavLink to="/">Accommodations</NavLink>
+            <NavLink to="/">Travel buddies</NavLink>
+            <NavLink to="/">Drivers</NavLink>
          </Box>
 
          <Box className="nav-section">
             <Box className="header">My Trips</Box>
-            <NavLink to="/guides">Create travel plan</NavLink>
-            <NavLink to="/guides">Upcoming trips</NavLink>
-            <NavLink to="/guides">Ended</NavLink>
+            <NavLink to="/">Create travel plan</NavLink>
+            <NavLink to="/">Upcoming trips</NavLink>
+            <NavLink to="/">Ended</NavLink>
          </Box>
       </Box>
    )
